f441f45a912e9cb7c996d33694c8ef13ac2fa922
Malondonof <marktigrelon@gmail.com>
2024-11-04 06:40:28 -0500

    App.py

--- pinturillo/App.py
+++ pinturillo/App.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
 st.set_page_config(page_title='Pintando con estilo', layout='wide')
-st.title('Vamos a dibujar algo bien MELOO')
+st.title('Haz una hermosa obra de arte')
 st.subheader("Dibuja en el panel")
 
 
