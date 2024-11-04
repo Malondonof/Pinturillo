f872eb74ab8e92e546aa7507860eff76f086849d
Malondonof <marktigrelon@gmail.com>
2024-11-04 06:42:04 -0500

    Update App.py

--- pinturillo/App.py
+++ pinturillo/App.py
@@ -3,7 +3,7 @@ from streamlit_drawable_canvas import st_canvas
 
 st.set_page_config(page_title='Arte', layout='wide')
 st.title('Haz una hermosa obra de arte')
-st.subheader("Dibuja en el panel")
+st.subheader("Dibuja en el lienzo")
 
 
 with st.sidebar:
